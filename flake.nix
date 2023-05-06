1f5ab20ee9951dd21cef9153a8191296fa63af9c
Kayla Firestack <git.dev@kaylafire.me>
2023-05-06 08:22:20 -0400

    fix indent

--- flake.nix
+++ flake.nix
@@ -5,23 +5,22 @@
 	outputs = { self, nixpkgs, utils }: {} // utils.lib.eachSystem ["x86_64-linux"] (system: let
 	 pkgs = import nixpkgs { inherit system;
 		overlays = [(final: prev: {
-		  buildGradle = final.callPackage ./gradle-env.nix {};
-
+			buildGradle = final.callPackage ./gradle-env.nix {};
 		})];
-	 };
-  in {
+		};
+	in {
 
-	 packages = rec {
-		cmdr-j = pkgs.callPackage ./default.nix {};
-		cmdr-j-standalone = pkgs.callPackage ./graalvm.nix {
-			inherit (self.packages.${system}) cmdr-j;
+		packages = rec {
+			cmdr-j = pkgs.callPackage ./default.nix {};
+			cmdr-j-standalone = pkgs.callPackage ./graalvm.nix {
+				inherit (self.packages.${system}) cmdr-j;
+			};
+			default = cmdr-j-nixpkgs-standalone;
 		};
 		cmdr-j-nixpkgs-standalone = pkgs.buildGraalvmNativeImage {
 			pname = "CommanderJ";
 			version = "0.1.0";
 			jar = cmdr-j;
 		};
-		default = cmdr-j-nixpkgs-standalone;
-	 };
-  });
+	});
 }
