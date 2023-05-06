ddfa843e5b6965c50cded05860a7b95e32c43d30
Kayla Firestack <git.dev@kaylafire.me>
2023-05-06 19:43:09 -0400

    fix:init: nix flake init -t github:ipetkov/crane#trunk

--- flake.nix
+++ flake.nix
@@ -9,64 +9,73 @@
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 
-		fenix = {
-			url = "github:nix-community/fenix";
-			inputs.nixpkgs.follows = "nixpkgs";
-			inputs.rust-analyzer-src.follows = "";
-		};
-
 		flake-utils.url = "github:numtide/flake-utils";
 
-		advisory-db = {
-			url = "github:rustsec/advisory-db";
-			flake = false;
+		rust-overlay = {
+			url = "github:oxalica/rust-overlay";
+			inputs = {
+				nixpkgs.follows = "nixpkgs";
+				flake-utils.follows = "flake-utils";
+			};
 		};
 	};
 
-	outputs = { self, nixpkgs, crane, fenix, flake-utils, advisory-db, ... }:
+	outputs = { self, nixpkgs, crane, flake-utils, rust-overlay, ... }:
 		flake-utils.lib.eachDefaultSystem (system:
 			let
 				pkgs = import nixpkgs {
 					inherit system;
+					overlays = [ (import rust-overlay) ];
 				};
 
 				inherit (pkgs) lib;
 
-				craneLib = crane.lib.${system};
-				src = craneLib.cleanCargoSource (craneLib.path ./.);
+				rustToolchain = pkgs.rust-bin.nightly.latest.default.override {
+					# Set the build targets supported by the toolchain,
+					# wasm32-unknown-unknown is required for trunk
+					targets = [ "wasm32-unknown-unknown" ];
+				};
+				craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+				# When filtering sources, we want to allow assets other than .rs files
+				src = lib.cleanSourceWith {
+					src = ./.; # The original, unfiltered source
+					filter = path: type:
+						(lib.hasSuffix "\.html" path) ||
+						(lib.hasSuffix "\.scss" path) ||
+						# Example of a folder for images, icons, etc
+						(lib.hasInfix "/assets/" path) ||
+						# Default filter from crane (allow .rs files)
+						(craneLib.filterCargoSources path type)
+					;
+				};
 
 				# Common arguments can be set here to avoid repeating them later
 				commonArgs = {
 					inherit src;
-
-					buildInputs = [
-						# Add additional build inputs here
-					] ++ lib.optionals pkgs.stdenv.isDarwin [
-						# Additional darwin specific inputs can be set here
-						pkgs.libiconv
-						pkgs.trunk
-					];
-
-					# Additional environment variables can be set directly
-					# MY_CUSTOM_VAR = "some value";
+					# We must force the target, otherwise cargo will attempt to use your native target
+					CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
 				};
-
-				craneLibLLvmTools = craneLib.overrideToolchain
-					(fenix.packages.${system}.complete.withComponents [
-						"cargo"
-						"llvm-tools"
-						"rustc"
-					]);
 
 				# Build *just* the cargo dependencies, so we can reuse
 				# all of that work (e.g. via cachix) when running in CI
-				cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+				cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {
+					# You cannot run cargo test on a wasm build
+					doCheck = false;
+				});
 
 				# Build the actual crate itself, reusing the dependency
 				# artifacts from above.
-				cmdr-rs = craneLib.buildPackage (commonArgs // {
+				# This derivation is a directory you can put on a webserver.
+				cmdr-rs = craneLib.buildTrunkPackage (commonArgs // {
 					inherit cargoArtifacts;
 				});
+
+				# Quick example on how to serve the app,
+				# This is just an example, not useful for production environments
+				serve-app = pkgs.writeShellScriptBin "serve-app" ''
+					${pkgs.python3Minimal}/bin/python3 -m http.server --directory ${cmdr-rs} 8000
+				'';
 			in
 			{
 				checks = {
@@ -74,7 +83,7 @@
 					inherit cmdr-rs;
 
 					# Run clippy (and deny all warnings) on the crate source,
-					# again, resuing the dependency artifacts from above.
+					# again, reusing the dependency artifacts from above.
 					#
 					# Note that this is done as a separate derivation so that
 					# we can block the CI if there are issues here, but not
@@ -84,58 +93,28 @@
 						cargoClippyExtraArgs = "--all-targets -- --deny warnings";
 					});
 
-					cmdr-rs-doc = craneLib.cargoDoc (commonArgs // {
-						inherit cargoArtifacts;
-					});
-
 					# Check formatting
 					cmdr-rs-fmt = craneLib.cargoFmt {
 						inherit src;
 					};
-
-					# Audit dependencies
-					cmdr-rs-audit = craneLib.cargoAudit {
-						inherit src advisory-db;
-					};
-
-					# Run tests with cargo-nextest
-					# Consider setting `doCheck = false` on `cmdr-rs` if you do not want
-					# the tests to run twice
-					cmdr-rs-nextest = craneLib.cargoNextest (commonArgs // {
-						inherit cargoArtifacts;
-						partitions = 1;
-						partitionType = "count";
-					});
-				} // lib.optionalAttrs (system == "x86_64-linux") {
-					# NB: cargo-tarpaulin only supports x86_64 systems
-					# Check code coverage (note: this will not upload coverage anywhere)
-					cmdr-rs-coverage = craneLib.cargoTarpaulin (commonArgs // {
-						inherit cargoArtifacts;
-					});
 				};
 
-				packages = {
-					default = cmdr-rs;
-					cmdr-rs-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
-						inherit cargoArtifacts;
-					});
-				};
+				packages.default = cmdr-rs;
 
 				apps.default = flake-utils.lib.mkApp {
-					drv = cmdr-rs;
+					drv = serve-app;
 				};
 
 				devShells.default = pkgs.mkShell {
-					inputsFrom = builtins.attrValues self.checks.${system};
-
-					# Additional dev-shell environment variables can be set directly
-					# MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+					inputsFrom = builtins.attrValues self.checks;
 
 					# Extra inputs can be added here
-					nativeBuildInputs = with pkgs; [
-						cargo
-						rustc
-						trunk
+					nativeBuildInputs = [
+						# cargo
+						# rustc
+						rustToolchain
+
+						pkgs.trunk
 					];
 				};
 			});
